b6b668172e84a74ad4725dd9b1a03d6186a68719
HERICK CEZAR DIAS ANDRADE <herick.cdandrade@senacsp.edu.br>
2022-05-30 08:22:56 -0300

    first commit

---
+++ exercicio.js
@@ -0,0 +1,12 @@
+/*let n1 = prompt("digite um numero")
+let n2 = prompt ("digite outro numero para a soma")
+let soma = parseInt(n1)+parseInt(n2)
+console.log("a soma foi de "+soma)
+*/
+let idade = prompt("Digite sua idade")
+
+if(idade >= 18){
+    alert("Você já pode votar")
+} else{
+    alert("você não pode votar")
+} 
